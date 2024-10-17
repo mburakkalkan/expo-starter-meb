b4a90ca0329eadaa12b38ad5e7449b1b381118b6
M. Burak Kalkan <mburakkalkan@gmail.com>
2024-10-17 11:58:23 +0300

    Create types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,42 @@
+declare module "*.jpg" {
+  import { ImageSourcePropType } from "react-native";
+  const content: ImageSourcePropType;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  import { ImageSourcePropType } from "react-native";
+  const content: ImageSourcePropType;
+  export default content;
+}
+
+declare module "*.png" {
+  import { ImageSourcePropType } from "react-native";
+  const content: ImageSourcePropType;
+  export default content;
+}
+
+declare module "*.bmp" {
+  import { ImageSourcePropType } from "react-native";
+  const content: ImageSourcePropType;
+  export default content;
+}
+
+declare module "*.gif" {
+  import { ImageSourcePropType } from "react-native";
+  const content: ImageSourcePropType;
+  export default content;
+}
+
+declare module "*.webp" {
+  import { ImageSourcePropType } from "react-native";
+  const content: ImageSourcePropType;
+  export default content;
+}
+
+declare module "*.svg" {
+  import { FC } from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: FC<SvgProps>;
+  export default content;
+}
