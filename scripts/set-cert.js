8b3f0ec1d196e4f429ae45f739d4696144a0041d
mburakkalkan <mburakkalkan@otobravo.com>
2025-09-26 15:04:28 +0300

    fix: update warning message for missing certificate and remove console log

--- scripts/set-cert.js
+++ scripts/set-cert.js
@@ -6,9 +6,8 @@ const certPath = path.resolve(__dirname, "../networking/MEB_SERTIFIKASI.crt");
 if (fs.existsSync(certPath)) {
 	if (!global._certSet) {
 		process.env.NODE_EXTRA_CA_CERTS = certPath;
-		console.log(`✅ NODE_EXTRA_CA_CERTS set to ${certPath}`);
 		global._certSet = true;
 	}
 } else {
-	console.warn("⚠️ MEB cert not found at networking/MEB_SERTIFIKASI.crt");
+	console.warn("⚠️ MEB_SERTIFIKASI.crt not found at", certPath);
 }
